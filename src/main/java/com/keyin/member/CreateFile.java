4709cf79503a4b92e9259a52be1d00bb2eec14ca
Michael Oreilly <michael.oreilly@keyin.com>
2022-06-20 17:01:38 -0230

    user Input for file name

--- src/main/java/com/keyin/member/CreateFile.java
+++ src/main/java/com/keyin/member/CreateFile.java
@@ -2,15 +2,20 @@ package com.keyin.member;
 
 import java.io.File;  // Import the File class
 import java.io.IOException;  // Import the IOException class to handle errors
+import java.util.Scanner;
 
 public class CreateFile {
 
   public static void main(String[] args) {
 
     String memberFile = "/Users/michaeloreilly/Desktop/VSCode/SD4/SDAT/Sprint1/sprint1/src/main/java/com/keyin/member/";
-    
+
     try {
-      File myObj = new File(memberFile + "filename.txt");
+        // user input for the file name
+        System.out.println("Enter the name of the file: ");
+        String fileName = new Scanner(System.in).nextLine();
+
+      File myObj = new File(memberFile + fileName + ".json");
       if (myObj.createNewFile()) {
         System.out.println("File created: " + myObj.getName());
       } else {
