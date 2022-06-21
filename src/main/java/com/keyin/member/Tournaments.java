7eee71c251c930f4d78e87c8acbd2064edbfa34f
Barry Poole <barry.poole@keyin.com>
2022-06-21 11:30:14 -0230

    Barry's Second Commit

--- src/main/java/com/keyin/member/Tournaments.java
+++ src/main/java/com/keyin/member/Tournaments.java
@@ -1,5 +1,12 @@
 package com.keyin.member;
 
+import java.util.*;
+
 public class Tournaments {
+
     private String tournamentName;
+    private String location;
+    private double entryFee;
+
+
 }
