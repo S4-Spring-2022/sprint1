3014ba5605160b3703ceab71fc244203c589dbe2
Gerald-Bartlett <96056847+Gerald-Bartlett@users.noreply.github.com>
2022-06-21 11:22:03 -0230

    Team1 project Tuesday June 21st

--- src/main/java/com/keyin/member/Member.java
+++ src/main/java/com/keyin/member/Member.java
@@ -1,7 +1,12 @@
 package com.keyin.member;
 
-import java.time.LocalDate;
+import java.time.*;
 import java.util.Date;
+import java.util.Locale;
+import java.time.LocalDate;
+import java.time.Month;
+
+import java.time.Period;
 
 public class Member {
     private String memberName;
@@ -10,16 +15,21 @@ public class Member {
     private String phoneNumber;
     private LocalDate startDate;
     private LocalDate duration;
-    private LocalDate todaysDate;
+//    private LocalDate todaysDate;
 
-    public Member(String memberName, String memberAddress, String email, String phoneNumber, LocalDate startDate, LocalDate duration){
+    public Member() {
+    }
+    public Member(String memberName, String memberAddress, String email, String phoneNumber, LocalDate startDate){
         this.memberName = memberName;
         this.memberAddress = memberAddress;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.startDate = startDate;
-        this.duration = duration;
+
     }
+
+
+
 
     public String getMemberName(){
         return memberName;
@@ -36,15 +46,24 @@ public class Member {
     public String getPhoneNumber(){
         return phoneNumber;
     }
-
+    public void  setStartDate(LocalDate date) {
+       this.startDate = date;
+}
     public LocalDate getStartDate(){
         return startDate;
     }
 
-    public LocalDate getTodaysDate(){
-        return  LocalDate today = LocalDate.now();
-
+    public LocalDate getDuration(){
+//        LocalDate startDate = LocalDate.of(2020, Month.JUNE, 21);
+        LocalDate today = LocalDate.now();
+        Period time = Period.between(startDate, today);
+        int years = time.getYears();
+        int months = time.getMonths();
+        int days = time.getDays();
+        System.out.println("Membership Duration: " + years + " year(s)"  + "," + months  + " Month(s)" + ","+ days + " Day(s) " );
+        return duration;
     }
+
 }
 
 
