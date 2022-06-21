47c57a7f4d3696b00e0df3f402671b85082050e4
DobbsZerker <nickdobbin@hotmail.com>
2022-06-21 09:09:53 -0230

    added memberclass and service class

--- src/main/java/com/keyin/member/Member.java
+++ src/main/java/com/keyin/member/Member.java
@@ -1,7 +1,76 @@
 package com.keyin.member;
-// Team -1 Barry_David_Gerald
 
-// testing branches
-// Testing again for the 3rd time
+import java.util.ArrayList;
+
+
 public class Member {
+    private String memberName;
+    private String memberAddress;
+    private String memberEmail;
+    private String memberPhone;
+    private String membershipStartDate;
+    private int membershipDuration;
+    private String membershipType;
+    private ArrayList<String> membershipFamilyMembers;
+    private ArrayList<String> currentTournaments;
+    private ArrayList<String> pastTournaments;
+    private ArrayList<String> upcomingTournaments;
+
+    public Member(String memberName, String memberAddress, String memberEmail, String memberPhone, String membershipStartDate, int membershipDuration, String membershipType, ArrayList<String> membershipFamilyMembers, ArrayList<String> currentTournaments, ArrayList<String> pastTournaments, ArrayList<String> upcomingTournaments) {
+        this.memberName = memberName;
+        this.memberAddress = memberAddress;
+        this.memberEmail = memberEmail;
+        this.memberPhone = memberPhone;
+        this.membershipStartDate = membershipStartDate;
+        this.membershipDuration = membershipDuration;
+        this.membershipType = membershipType;
+        this.membershipFamilyMembers = membershipFamilyMembers;
+        this.currentTournaments = currentTournaments;
+        this.pastTournaments = pastTournaments;
+        this.upcomingTournaments = upcomingTournaments;
+    }
+
+    public String getMemberName() {
+        return this.memberName;
+    }
+
+    public String getMemberAddress() {
+        return this.memberAddress;
+    }
+
+    public String getMemberEmail() {
+        return this.memberEmail;
+    }
+
+    public String getMemberPhone() {
+        return this.memberPhone;
+    }
+
+    public String getMembershipStartDate() {
+        return this.membershipStartDate;
+    }
+
+    public int getMembershipDuration() {
+        return this.membershipDuration;
+    }
+
+    public String getMembershipType() {
+        return this.membershipType;
+    }
+
+    public ArrayList<String> getMembershipFamilyMembers() {
+        return this.membershipFamilyMembers;
+    }
+
+    public ArrayList<String> getCurrentTournaments() {
+        return this.currentTournaments;
+    }
+
+    public ArrayList<String> getPastTournaments() {
+        return this.pastTournaments;
+    }
+
+    public ArrayList<String> getUpcomingTournaments() {
+        return this.upcomingTournaments;
+    }
 }
--- src/main/java/com/keyin/member/MemberService.java
+++ src/main/java/com/keyin/member/MemberService.java
@@ -1,4 +1,40 @@
 package com.keyin.member;
 
-public class MemberService {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService implements MemberDal {
+
+    private static List<Member> DB = new ArrayList();
+
+    public MemberService() {
+    }
+
+    public int insertMember(Member member) {
+        DB.add(new Member(member.getMemberName(), member.getMemberAddress(), member.getMemberEmail(), member.getMemberPhone(), member.getMembershipStartDate(), member.getMembershipDuration(), member.getMembershipType(), member.getMembershipFamilyMembers(), member.getCurrentTournaments(), member.getPastTournaments(), member.getUpcomingTournaments()));
+        return 1;
+    }
+
+    public List<Member> selectAllMembers() {
+        return DB;
+    }
+
+    public Optional<Member> selectMemberByName(String memberName) {
+        return DB.stream().filter((member) -> {
+            return member.getMemberName().equals(memberName);
+        }).findFirst();
+    }
+
+    public Optional<Member> selectMemberByEmail(String memberEmail) {
+        return DB.stream().filter((member) -> {
+            return member.getMemberEmail().equals(memberEmail);
+        }).findFirst();
+    }
+
+    public Optional<Member> selectMemberByPhone(String memberPhone) {
+        return DB.stream().filter((member) -> {
+            return member.getMemberPhone().equals(memberPhone);
+        }).findFirst();
+    }
 }
