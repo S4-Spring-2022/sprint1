a5e7afe45c4301f8bf14c71c0455bf6683f6f80a
ashleylinda90 <ashley.fillmore@keyin.com>
2022-06-21 12:34:15 -0230

    Ashley commit Membership class

--- src/main/java/com/keyin/member/Membership.java
+++ src/main/java/com/keyin/member/Membership.java
@@ -5,18 +5,26 @@ import java.util.Date;
 public class Membership {
 
     private String type;
-    private Date StartDate;
+    private Date startDate;
     private String duration;
 
     public Membership() {
-        this.type = "";
-        this.StartDate = new Date();
-        this.duration = "";
+        this.type = "Family Plan";
+        if (this.type.equals("Family Plan")) {
+            System.out.println("Family members included are ???");
+        }
+
+        this.startDate = new Date();
+        this.duration = ""; // current date subtract start date?
     }
 
     public Membership(String type, String duration) {
         this.type = type;
-        this.StartDate = new Date();
+        this.startDate = new Date();
         this.duration = duration;
+    }
+
+    public String toString() {
+        return "Membership[Type: " + type + ", Start Date: " + startDate + ", Duration: " + duration + "]";
     }
 }
