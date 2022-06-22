89466d4b7556aee9e3bce17ad1803340da2d9825
Michael Oreilly <michael.oreilly@keyin.com>
2022-06-22 13:19:30 -0230

    small changes to ObjectToJsonFile

--- src/main/java/com/keyin/member/CRUD/ObjectToJsonFile.java
+++ src/main/java/com/keyin/member/CRUD/ObjectToJsonFile.java
@@ -13,17 +13,24 @@ public class ObjectToJsonFile {
 
         // String memberDataFolder = "./src/main/java/com/keyin/member/data/";
         String memberDataFolder = "/Users/michaeloreilly/Desktop/VSCode/SD4/SDAT/Sprint1/sprint1/src/main/java/com/keyin/member/data/";
+        // String file = "/Users/michaeloreilly/Desktop/VSCode/SD4/SDAT/Sprint1/sprint1/src/main/java/com/keyin/member/data/memberData3/";
 
-
-        Member member = new Member();
+        // hardcoded member data
+        Member member = new Member(); // need to figure out how to include the member class
+        // member.setClass();
         member.setName("Sherlock Holmes");
         member.setAddress("221B Baker Street");
         member.setEmail("s.holmes@mysterysolvers.com");
         member.setPhone(4207777);
 
+        // add memberInfo to an memberArray
+
+
+        // user inputs for member data
+
         ObjectMapper mapper = new ObjectMapper();
 
-        File file = new File(memberDataFolder + "memberData3.json");
+        File file = new File(memberDataFolder + "memberData3.json"); // is creating new file
         try {
             // Serialize Java object info JSON file.
             mapper.writeValue(file, member);
@@ -34,8 +41,10 @@ public class ObjectToJsonFile {
         try {
             // Deserialize JSON file into Java object.
             Member newMember = mapper.readValue(file, Member.class);
-            System.out.println("=== New member added sussfully ===");
             System.out.println();
+            System.out.println("=== Add New Member ( Successful ) ===");
+            System.out.println();
+            // System.out.println("Class: " + newMember.getClass());
             System.out.println("Name: " + newMember.getName());
             System.out.println("Address: " + newMember.getAddress());
             System.out.println("Email: " + newMember.getEmail());
