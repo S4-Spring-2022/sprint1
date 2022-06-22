3122cd414bdd8a06111b285afb3fa14c22c5eb2b
DavidJTemple <96357555+DavidJTemple@users.noreply.github.com>
2022-06-22 17:05:09 -0230

    Updates

--- src/main/java/com/keyin/member/Database.java
+++ src/main/java/com/keyin/member/Database.java
@@ -7,42 +7,8 @@ import java.util.List;
 
 public class Database extends MembershipType {
 
-    public List<MembershipType> getMembershipTypes() {
-        ArrayList<MembershipType> membershipTypes = new ArrayList<>();
 
 
-//
-//        MembershipType normal = new MembershipType();
-//        normal.setId(1);
-//        normal.setMonthlyCost(61.75);
-//        normal.add(normal);
-//
-//        MembershipType trial = new MembershipType();
-//        membershipTypes.setId(2);
-//        membershipTypes.setMonthlyCost(70);
-//        membershipTypes.add(trial);
-//
-//        MembershipType specialOffer = new MembershipType();
-//        membershipTypes.setId(3);
-//        membershipTypes.setMonthlyCost(52.75);
-//        membershipTypes.add(specialOffer);
-//
-//        MembershipType familyPlan = new MembershipType();
-//        membershipmembershipTypesTypeOf.setId(4);
-//        membershipTypes.setMonthlyCost(52.75);
-//        membershipTypes.addMember1("");
-//        membershipTypes.add(familyPlan);
-//
-//
-//
-        return membershipTypes;
-//    }
-//
-//    public static void main(String[] args) {
-//        System.out.println("Print Arraylist using for loop");
-//        for(int i=0; i < MembershipType.length(); i++){
-//            System.out.println( MembershipType.get(i) );
-    }
 
 }
 
--- src/main/java/com/keyin/member/MemberServices.java
+++ src/main/java/com/keyin/member/MemberServices.java
@@ -1,12 +1,14 @@
 package com.keyin.member;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-public class MemberServices {
+public class MemberServices extends MemberDatabase {
 
 
     public static void main(String[] args) {
-        String option;
+        int option;
         {
 
             System.out.println("Option Menu");
@@ -14,29 +16,28 @@ public class MemberServices {
                     "3. Update Account\n 4. Delete Account\n");
             System.out.println();
 
-            Scanner input = new Scanner(System.in);
             System.out.println("Insert your option:");
-            option = input.nextLine();
+            MemberDatabase memberInstance = new MemberDatabase();
+            memberInstance.allMembers();
+            Scanner input = new Scanner(System.in);
+
+            option = input.nextInt();
             input.close();
-
-            String userOption;
-            switch (option) {
-
-                case "1":
-                    userOption = "You would like to add a new member";
-                case "2":
-                    userOption = "You would like to view account";
-
-                case "3":
-                    userOption = "You would like to update account";
-
-                default:
-                    userOption = "You would like to Delete an account";
-                    break;
+            if (option == 1){
+                System.out.println("You've chose to create a new member");
+            } else if (option == 2) {
+                System.out.println("You've chosen to view an account");
+                System.out.println();
+                System.out.println("Please enter Last Name");
 
 
+            } else if (option == 3) {
+                System.out.println("You've chose to update an existing account");
+            } else if (option == 4) {
+                System.out.println("You've chosen to delete an account");
+            }else {
+                System.out.println("Invalid Selection");
             }
-            System.out.println(userOption);
         }
     }
     }
