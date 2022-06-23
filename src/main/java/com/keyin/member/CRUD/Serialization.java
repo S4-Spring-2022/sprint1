4e4491be06713e272e05fd6d9e7b1e94b94a0c05
Michael Oreilly <michael.oreilly@keyin.com>
2022-06-23 11:05:06 -0230

    Serialization for single member

--- src/main/java/com/keyin/member/CRUD/Serialization.java
+++ src/main/java/com/keyin/member/CRUD/Serialization.java
@@ -1,27 +1,39 @@
 package com.keyin.member.CRUD;
 // writing the file
 
+import java.io.File;
 import java.io.IOException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.keyin.member.Member;
 
 public class Serialization {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
-        String pathName = "/Users/michaeloreilly/Desktop/VSCode/SD4/SDAT/Sprint1/sprint1/src/main/java/com/keyin/member/data/memberData.json";
+        String pathName = "sprint1/src/main/java/com/keyin/member/data/memberData.json";
         
-        try {
         ObjectMapper mapper = new ObjectMapper();
         
-        Member member = new Member();
+        try {
+            Member member = new Member();
+
                member.setName("Sherlock Holmes");
                member.setAddress("221B Baker Street");
                member.setEmail("s.holmes@mysterysolvers.com");
                member.setPhone(4207777);
 
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            String memberInfo = mapper.writeValueAsString(member);
+            
+            mapper.writeValue( new File(pathName), member );
+
+            System.out.println();
+            System.out.println("Serialized member to JSON file");
+            System.out.println(memberInfo);
+            System.out.println();
+            
+
+        } catch (IOException e) {
+            e.printStackTrace();
+       }
     }
 }
