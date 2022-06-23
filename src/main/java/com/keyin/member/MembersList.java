cecc5c832152c9502fcb5f7fb468f6c54282302c
RoderickWells <RoderickWells@users.noreply.github.com>
2022-06-22 22:50:03 -0230

    updated MembersList Class

--- src/main/java/com/keyin/member/MembersList.java
+++ src/main/java/com/keyin/member/MembersList.java
@@ -18,19 +18,19 @@ public class MembersList {
         label.setBounds(350, 10, 200, 30);
         label.setFont(new Font("Serif", Font.BOLD, 20));
 
-        String[] columnNames = { "Name", "Address", "Email", "Phone", "StartDate", "Duration(Years)", "Type" };
+        String[] columnNames = { "Name", "Address", "Email", "Phone", "StartDate", "Duration(Years)", "Type", "CurrTour","PastTour","UpcomingTour" };
         Object[][] data = { { "John", "123 Main St", "john@email.ca", "123-456-7890", "01/01/2019", "1", "Normal" },
-                { "Jane", "243 Main St", "jane@email.ca", "123-456-7890", "01/01/2019", "1", "Normal" }, };
+                { "Jane", "243 Main St", "jane@email.ca", "123-456-7890", "01/01/2019", "1", "Normal", "Annual Tourney" } };
 
         table = new JTable(data, columnNames);
         scrollPane = new JScrollPane(table);
-        scrollPane.setBounds(10, 50, 1000, 1000);
+        scrollPane.setBounds(10, 50, 1600, 800);
 
         frame.add(label);
         frame.add(scrollPane);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(350, 280);
+        frame.setSize(1000, 800);
         frame.setLayout(null);
         frame.setVisible(true);
 
