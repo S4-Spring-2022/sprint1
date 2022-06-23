ad06a77554d24284df3d27eb406d329e4b36cf33
Gerald-Bartlett <96056847+Gerald-Bartlett@users.noreply.github.com>
2022-06-23 12:36:09 -0230

    Team1 project Tuesday Pm June 21st

--- src/main/java/com/keyin/member/MemberServices.java
+++ src/main/java/com/keyin/member/MemberServices.java
@@ -10,20 +10,12 @@ public class MemberServices extends MemberDatabase {
     public static void main(String[] args) {
         int option;
         {
-
             System.out.println("Option Menu");
             System.out.println("1. Add new member\n 2. View Account\n " +
                     "3. Update Account\n 4. Delete Account\n");
             System.out.println();
-
             System.out.println("Insert your option:");
-
-
-            // Non-Static to static data
-            MemberDatabase memberInstance = new MemberDatabase();
-            memberInstance.allMembers();
             Scanner input = new Scanner(System.in);
-
             option = input.nextInt();
             input.close();
             if (option == 1){
@@ -32,8 +24,13 @@ public class MemberServices extends MemberDatabase {
                 System.out.println("You've chosen to view an account");
                 System.out.println();
                 System.out.println("Please enter Last Name");
-
-
+                // Non-Static to static data
+                String answer;
+                MemberDatabase memberInstance = new MemberDatabase();
+                Scanner memberScan = new Scanner(System.in);
+                System.out.println(memberInstance.allMembers());
+                memberInstance.allMembers();
+                memberScan.close();
             } else if (option == 3) {
                 System.out.println("You've chose to update an existing account");
             } else if (option == 4) {
